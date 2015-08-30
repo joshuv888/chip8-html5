fdcd679765b7da717e00a32f17816251971a60b1
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2015-08-30 17:29:53 +0000

    Update chip8.js. TODO comments. modified:   js/chip8.js * TODO comments in "CPU->cycle". * "CPU->cycle" variables.

--- js/chip8.js
+++ js/chip8.js
@@ -19,6 +19,12 @@ var CHARACTER_HEIGHT = 5; // 5 lines to draw a number into screen.
 var NUM_BITS = 0x08; // 8 bits in a byte. 
 var BYTE_MASK = 0xFF; // 255 number as 8 bits mask. 
 
+// Make a word number. 
+var toWord = ( function( highByte, lowerByte){ 
+    
+    return ( highByte << NUM_BITS ) | ( lowerByte & BYTE_MASK ); 
+    
+} ); 
 
 // Listener pattern
 var Listenable = ( function(  ){ 
@@ -110,13 +116,6 @@ var Chip8 = ( function( guiInterface ){
             
         } ); 
         
-        // Make a word number. 
-        var toWord = ( function( highByte, lowerByte){ 
-            
-            return ( highByte << NUM_BITS ) | ( lowerByte & BYTE_MASK ); 
-            
-        } ); 
-        
         
         var data = new Uint8Array( DISPLAY_ARRAY_SIZE ); // 2048 bits. 
         
@@ -170,7 +169,53 @@ var Chip8 = ( function( guiInterface ){
         // Emulate the cycle of CPU. 
         this.cycle = ( function(  ){ 
             
-            // TODO
+            /*
+                
+                TODO 
+                
+                00E0    Clears the screen. 
+                00EE    Returns from a subroutine. 
+                1NNN    Jumps to address NNN. 
+                2NNN    Calls subroutine at NNN. 
+                3XNN    Skips the next instruction if VX equals NN. 
+                4XNN    Skips the next instruction if VX doesn't equal NN. 
+                5XY0    Skips the next instruction if VX equals VY. 
+                6XNN    Sets VX to NN. 
+                7XNN    Adds NN to VX. 
+                8XY0    Sets VX to the value of VY. 
+                8XY1    Sets VX to VX or VY. 
+                8XY2    Sets VX to VX and VY. 
+                8XY3    Sets VX to VX xor VY. 
+                8XY4    Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't. 
+                8XY5    VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't. 
+                8XY6    Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift. 
+                8XY7    Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't. 
+                8XYE    Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift. 
+                9XY0    Skips the next instruction if VX doesn't equal VY. 
+                ANNN    Sets I to the address NNN. 
+                BNNN    Jumps to the address NNN plus V0. 
+                CXNN    Sets VX to the result of a bitwise and operation on a random number and NN. 
+                DXYN    Sprites stored in memory at location in index register (I), 8bits wide. Wraps around the screen. If when drawn, clears a pixel, register VF is set to 1 otherwise it is zero. All drawing is XOR drawing (i.e. it toggles the screen pixels). Sprites are drawn starting at position VX, VY. N is the number of 8bit rows that need to be drawn. If N is greater than 1, second line continues at position VX, VY+1, and so on. 
+                EX9E    Skips the next instruction if the key stored in VX is pressed. 
+                EXA1    Skips the next instruction if the key stored in VX isn't pressed. 
+                FX07    Sets VX to the value of the delay timer. 
+                FX0A    A key press is awaited, and then stored in VX. 
+                FX15    Sets the delay timer to VX. 
+                FX18    Sets the sound timer to VX. 
+                FX1E    Adds VX to I. 
+                FX29    Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font. 
+                FX33    Stores the Binary-coded decimal representation of VX, with the most significant of three digits at the address in I, the middle digit at I plus 1, and the least significant digit at I plus 2. (In other words, take the decimal representation of VX, place the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.) 
+                FX55    Stores V0 to VX in memory starting at address I. 
+                FX65    Fills V0 to VX with values from memory starting at address I. 
+                
+            */
+            
+            var opcode = toWord( memory[ programCounter++ ], memory[ programCounter++ ] ); // Gets current instruction. 
+            var x_op = ( opcode & 0x0F00 ) >> 8; // Gets third hexadecimal digit. 
+            var y_op = ( opcode & 0x00F0 ) >> 4; // Gets second hexadecimal digit. 
+            var value_op = ( opcode & BYTE_MASK ); // Gets lower byte of "opcode". 
+            var address_op = ( opcode & 0x0FFF ); // Get a 12-bit value used as the memory address. 
+            
             
         } ); 
         
@@ -221,7 +266,7 @@ var Chip8 = ( function( guiInterface ){
     
     // Private chip8 interface. 
     var chip8Interface = ( { 
-        // TODO: waitKey, isKeyPressed. 
+        // TODO: waitKey, isKeyPressed, setDrawFlag. 
     } ); 
     
     
